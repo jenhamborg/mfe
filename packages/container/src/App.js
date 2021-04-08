f43f902044f978d9f8af692ca887b0dfa4dfcc1b
Jenny Hamborg <j.hamborg@acustaf.com>
2021-04-08 08:56:22 -0500

    initial commit

---
+++ packages/container/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import MarketingApp from './components/MarketingApp';
+
+
+
+export default () => {
+    return (
+    <>
+        <div>Hi There</div>
+        <hr />
+        <MarketingApp />
+    </>
+    )
+}
