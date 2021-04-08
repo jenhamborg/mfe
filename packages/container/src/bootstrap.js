31fbb1f45d442a8a0d1be52b68dd9dc1fd397ab2
Jenny Hamborg <j.hamborg@acustaf.com>
2021-04-08 10:53:51 -0500

    testing config

--- packages/container/src/bootstrap.js
+++ packages/container/src/bootstrap.js
@@ -2,4 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
+// bootstrap imported to index.js
+
 ReactDOM.render(<App />, document.querySelector('#root'));
