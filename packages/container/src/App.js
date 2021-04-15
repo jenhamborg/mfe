792cce6a7df524e28d9e0ae7e801cb61b2404001
Jenny Hamborg <j.hamborg@acustaf.com>
2021-04-15 11:36:05 -0500

    added suspense and lazy loading

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,23 +1,33 @@
-import React from 'react';
-import { BrowserRouter } from 'react-router-dom';
-import { StylesProvider, createGenerateClassName } from '@material-ui/core/styles';
+import React, { lazy, Suspense } from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import {
+  StylesProvider,
+  createGenerateClassName,
+} from '@material-ui/core/styles';
 
-import MarketingApp from './components/MarketingApp';
 import Header from './components/Header';
 
-const generateClassName = createGenerateClassName ({
-    productionPrefix: 'co',
-  });
+const MarketingLazy = lazy(() => import('./components/MarketingApp'));
+const AuthLazy = lazy(() => import('./components/AuthApp'));
+
+const generateClassName = createGenerateClassName({
+  productionPrefix: 'co',
+});
 
 export default () => {
-    return (
-    <StylesProvider generateClassName={generateClassName}>
-        <BrowserRouter>
-            <div>
-                <Header />
-                <MarketingApp />
-            </div>
-        </BrowserRouter>
-    </StylesProvider>
-    )
-}
+  return (
+    <BrowserRouter>
+      <StylesProvider generateClassName={generateClassName}>
+        <div>
+          <Header />
+          <Suspense fallback={<div>Loading....</div>}>
+            <Switch>
+              <Route path="/auth" component={AuthLazy} />
+              <Route path="/" component={MarketingLazy} />
+            </Switch>
+          </Suspense>
+        </div>
+      </StylesProvider>
+    </BrowserRouter>
+  );
+};
